bb96837c4503316fb7a5142d5b82c6d1b59dd7da
Rolel <rolel@smile.fr>
2023-12-30 00:13:44 +0100

    Setup project

---
+++ forge.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['linux'],
+    },
+      /*
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+      */
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+  ],
+};
